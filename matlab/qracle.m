706a9505bec3fa1dafc9a5783a6b6301e3573af5
Usio Haruka <70017686+Citron-kyo@users.noreply.github.com>
2022-05-27 15:26:56 +0800

    Add files via upload

---
+++ matlab/qracle.m
@@ -0,0 +1,17 @@
+clc;
+clear;
+C=[];
+rnd=[];
+size=256;
+roll=size^2/1024;
+rollm=roll;
+rollm(rollm>1024)=1024;
+for i=1:1:ceil(roll/1024)
+    rnd=[rnd;cell2mat(webread(['https://qrng.anu.edu.au/API/jsonI.php?length=',int2str(rollm),'&type=hex16&size=1024']).data)];
+end
+for i=1:1:1024
+    C(:,i)=hex2dec(rnd(:,(2*i-1):(2*i)));
+end
+C=reshape(C,size,size);
+I=mat2gray(C);
+imshow(I);
